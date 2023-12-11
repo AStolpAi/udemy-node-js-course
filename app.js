2b9747f5b78a118d0573f61e940aa4d8f8fd91b3
AnthonyStolp <anthony.stolp@contractor.prometric.com>
2023-12-11 15:20:49 -0500

    added expressjs framework

--- app.js
+++ app.js
@@ -1,33 +1,20 @@
+//Core Node Modules
 const http = require('http');
-const fs = require("fs");
 
-const server = http.createServer((req, res ) => {
-    const url = req.url;
-    const method = req.method;
+//Third Party
+const express = require('express');
 
-    if( url === '/') {
-        res.write('<html>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
-        res.write('</html>');
-        return res.end();
-    }
-    if(url === "/message" && method === "POST") {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.writeHead(302, {'Location': '/'})
-    }
-    res.write('<html>');
-        res.write('<body><h1>Hello from my nodejs file!</h1></body>')
-        res.write('</html>');
-        return res.end();
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("anthony");
+    next();
 });
+
+app.use((req, res, next) => {
+    console.log("I am the next middleware function");
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
