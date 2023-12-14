4b79e75b3f19f6dd3dac84cc41ed8d3e26b1024c
AnthonyStolp <anthony.stolp@contractor.prometric.com>
2023-12-14 09:56:54 -0500

    wrapped up templating with ejs, removed all others

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 
 const adminData =require('./routes/admin');
@@ -16,12 +17,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'views', "404.html"));
-    res.status(404).render('404', {pageTitle: '404 error. Please Try again', errorMessage: 'Sorry but this page does not exist.'})
+    res.status(404).render('404', {pageTitle: '404', errorMessage: 'Sorry but this page does not exist.'})
 });
 
 app.listen(3000);
