da95a93a5aa24bc7b8ee99e0239929a46f177e4b
AnthonyStolp <anthony.stolp@contractor.prometric.com>
2023-12-15 16:42:45 -0500

    resolved bug

--- models/product.js
+++ models/product.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const p = path.join(path.dirname(require.main.filename, "data", "products.json"));
+const p = path.join(path.dirname(require.main.filename), "data", "products.json");
+
+console.log(p, "pppppp");
 
 const getProductsFromFile = (callback) => {
     fs.readFile(p, (err, fileContent) => {
@@ -13,15 +15,20 @@ const getProductsFromFile = (callback) => {
 }
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
         getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
+                if(err) {
+                    console.log(err);
+                }
             });
         });
     }
